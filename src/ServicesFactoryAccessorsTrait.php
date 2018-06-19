7be29740bb3ed5038ad6e6e893e8db615d9ffaa7
LouisCuny <louis-cuny@orange.fr>
2018-06-19 14:32:58 +0200

    removed

--- src/ServicesFactoryAccessorsTrait.php
+++ src/ServicesFactoryAccessorsTrait.php
@@ -2,6 +2,11 @@
 /**
  * This file is part of the Objective PHP project
  *
+
+
+
+
+ 
  * More info about Objective PHP on www.objective-php.org
  *
  * @license http://opensource.org/licenses/GPL-3.0 GNU GPL License 3.0
